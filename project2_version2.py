98de9371244181c2d9464eb45a2808dd40d15148
baewood <46425553+baewood@users.noreply.github.com>
2019-10-15 15:35:01 -0500

    Update project2_version2.py

--- project2_version2.py
+++ project2_version2.py
@@ -2,16 +2,20 @@
 
 def loan_amount():
     initial_loan = float(input("Please provide the requested loan amount: $"))
+    
     while initial_loan < 500:
         print("We're sorry, but we do not finance loans under $500.\n")
         initial_loan = float(input("Please provide the requested loan amount: $"))
+        
     return initial_loan 
     
 def number_payments(initial_loan):
     number_payment = int(input("Please enter how many payments you would like to make on this loan: "))
+    
     while number_payment < 6 or number_payment > 48:
         print("We're sorry, but the minimum amount of payemnts is 6 months.\n")
         number_payment = int(input("Please enter how many payments you would like to make on this loan: "))
+        
     return number_payment 
     
 def interest_rates(initial_loan, number_payment):
