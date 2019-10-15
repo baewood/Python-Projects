61716286e205e82a8a03a958bb1d77c773eb332b
baewood <46425553+baewood@users.noreply.github.com>
2019-10-15 14:06:56 -0500

    Update Wood_ProgProj_2.py

--- Wood_ProgProj_2.py
+++ Wood_ProgProj_2.py
@@ -1,7 +1,6 @@
 # This project calculates loans using functions and loops.
 # Description: Programming Project 2
 
-
 def loan_amount():
     initial_loan = float(input("Please provide the requested loan amount: $"))
     while initial_loan < 500:
@@ -43,23 +42,27 @@ def interest_rates(initial_loan, number_payment):
 
 def repeat_program():
     repeat = input("Would you like to repeat the program? Y for yes, N for no: ")
+
     while repeat == "Y" or repeat == "y":
-        initial_loan = float(input("Please provide the requested loan amount: $"))
-        number_payment = int(input("Please enter how many payments you would like to make on this loan: "))
+        loan = loan_amount()
+        payment = number_payments(loan)
+        iRate = interest_rates(loan, payment)
         repeat = input("Would you like to repeat the program? Y for yes, N for no: ")
 
-        while initial_loan < 500:
+        if loan < 500:
             print("We're sorry, but we do not finance loans under $500.\n")
-            initial_loan = float(input("Please provide the requested loan amount: $"))
-            number_payment = int(input("Please enter how many payments you would like to make on this loan: "))
-            repeat = input("Would you like to repeat the program? Y for yes, N for no: ")
+            loan = float(input("Please provide the requested loan amount: $"))
+            payment = int(input("Please enter how many payments you would like to make on this loan: "))
 
-            while number_payment < 6 or number_payment > 48:
-                print("We're sorry, but the minimum amount of payemnts is 6 months.\n")
-                number_payment = int(input("Please enter how many payments you would like to make on this loan: "))
-                repeat = input("Would you like to repeat the program? Y for yes, N for no: ")
-
-    return repeat_program
+        if payment < 6 or payment > 48:
+            print("We're sorry, but the minimum amount of payemnts is 6 months.\n")
+            payment = int(input("Please enter how many payments you would like to make on this loan: "))
+    
+    monthly_p = (loan * iRate * (1 + iRate) * payment / ((1 + iRate) * payment - 1))
+    rate = iRate * 100
+    
+    print("Loan Amount: ${:4.2f} , Number of Payments: {:3d} , Interest Rate: {:3.2f}% , Monthly Payment of: ${:3.2f} ".format(loan, payment, rate, monthly_p))
+    
     
 def main():
     loanAmount = loan_amount()
@@ -73,11 +76,4 @@ def main():
 
     repeat_program()
 
-    monthly = (loanAmount * interestRate * (1 + interestRate) * numofPayments / ((1 + interestRate) * numofPayments - 1))
-    IR = interestRate * 100
-    
-
-    print("Loan Amount: ${:4.2f} , Number of Payments: {:3d} , Interest Rate: {:3.2f}% , Monthly Payment of: ${:3.2f} ".format(loanAmount, numofPayments, IR, monthly))
-
 main()
-
